04c923d039832bc9d976e8ba38359f2d204dcf82
Rob <robpolisena@gmail.com>
2020-07-01 23:06:33 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,34 +1,32 @@
 //  Pairing with @Oluwatobii
 
-const transpose = function (matrix) {
-    // Put your solution here
-    let newMatrix = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-      let col = [];
-      for (let j = 0; j < matrix.length; j++) {
-        col.push(matrix[j][i]);
-      }
-      newMatrix.push(col);
+const transpose = function(matrix) {
+  let newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let col = [];
+    for (let j = 0; j < matrix.length; j++) {
+      col.push(matrix[j][i]);
     }
-    return newMatrix;
-  };
+    newMatrix.push(col);
+  }
+  return newMatrix;
+};
 
-
-const wordSearch = (letters, word) => { 
-  const horizontalJoin = letters.map(ls => ls.join(''))
-  for (l of horizontalJoin) {
-      if (l.includes(word)) {
-          return true
-      } 
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
   }
   const verticalLetters = transpose(letters);
-  const verticalJoin = verticalLetters.map(ls => ls.join(''))
-  for (l of verticalJoin) {
+  const verticalJoin = verticalLetters.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
     if (l.includes(word)) {
-        return true
-    } 
+      return true;
+    }
   }
-  return false
-}
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
